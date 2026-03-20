399ee992c5df3fe222bf5678ac7bf27e7487542c
Rohit Khati <rohitkhati303@gmail.com>
2026-03-20 23:31:31 +0530

    added rateLimiter

--- server/middleware/rateLimiter.middleware.js
+++ server/middleware/rateLimiter.middleware.js
@@ -1,6 +1,49 @@
 //import rateLimite
-//register rate limiter 3 attempts per hrs
 
-//login rate limite 5 attempts per 15 mintues 
+import rateLimit from "express-rate-limit";
+
+//register rate limiter 3 attempts per hrs
+export const registerLimit = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  max: 3,
+  message: {
+    sucees: false,
+    message: "Too many account created. Please try again after some time",
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+//login rate limite 5 attempts per 15 mintues
+export const loginLimit = rateLimit({
+  windowMs: 15 * 60 * 1000, //15 minutes
+  max: 5, //only 5 attempts
+  message: {
+    success: false,
+    message: "Too many login attempts. Please try agian afte 15 mintues",
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
 //otp rate limit 3 attempts per hrs
-// forgot password rate limit 3 attempts per hrs 
+export const optlimit = rateLimit({
+  windowMs: 60 * 60 * 1000, // 1 hours
+  max: 3,
+  message: {
+    success: false,
+    message: "Too many otp request. Please try again after an hour",
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+// forgot password rate limit 3 attempts per hrs
+export const forgotPasswordLimit = rateLimit({
+  windowMs: 60 * 60 * 1000, // 1 hour
+  max: 3,
+  message: {
+    success: false,
+    message: "Too many password request. Please try agian after an hour",
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { register, login, logout  } from "../controllers/auth.controllers.js";
 import { verifyToken } from "../middleware/auth.middleware.js";
+import { loginLimit, registerLimit } from "../middleware/rateLimiter.middleware.js";
 
 const router = Router();
 
 //POST /api/auth/register --- no token needed 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register", registerLimit, register);
+router.post("/login", loginLimit, login);
 
 //This is protected route- token required 
 router.post("/logout", verifyToken , logout);
